1adb2426288156064fd9190fccac68145ba9c005
liubomyr <debryniuk@gmail.com>
2020-03-15 23:44:50 +0200

    dao was added

--- src/com/patterns/DAODemo.java
+++ src/com/patterns/DAODemo.java
@@ -1,4 +1,35 @@
 package com.patterns;
 
 public class DAODemo {
+    static DB storage = new DB();
+
+    public static void main(String[] args) {
+        System.out.println(storage.getData());
+    }
+}
+
+interface Storage{
+    String getData();
+}
+
+class DB implements Storage{
+    String getFromTable(){
+        return "data from table";
+    }
+
+    @Override
+    public String getData() {
+        return getFromTable();
+    }
+}
+
+class FileSystem implements Storage {
+    String getFromFs(){
+        return "data from fs";
+    }
+
+    @Override
+    public String getData() {
+        return getFromFs();
+    }
 }
